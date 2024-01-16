790aef4b775aee8fcf0578f45ad1f2e331418022
Balder Bongaerts <bbongaerts@gmail.com>
2024-01-16 16:31:56 +0100

    feat: add v1 login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {CreateClassgroupComponent} from "./pages/create-classgroup/create-classgroup.component";
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-  {path: 'create-classgroup', component: CreateClassgroupComponent}
+  {path: 'create-classgroup', component: CreateClassgroupComponent},
+  {path: 'login', component: LoginComponent},
 ];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatIconModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide: boolean = true; 
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+}
