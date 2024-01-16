c9a4c141d84984d585f6fcf55e2cbb4b5af10a2e
Thomas Artois <thomas_artois99@hotmail.com>
2024-01-16 16:16:13 +0100

    button component but not working

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatDividerModule} from "@angular/material/divider";
@@ -6,6 +6,7 @@ import {MatDividerModule} from "@angular/material/divider";
 @Component({
   selector: 'app-button',
   standalone: true,
+  inputs: ['buttonName'],
   imports: [
     MatButtonModule, MatDividerModule, MatIconModule
   ],
@@ -13,5 +14,6 @@ import {MatDividerModule} from "@angular/material/divider";
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
+  @Input() buttonName: string = '';
 
 }
