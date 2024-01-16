8b1213a9e9e782754a03557da05cfc0dcd21d71e
Thomas Artois <thomas_artois99@hotmail.com>
2024-01-16 15:49:21 +0100

    button component created

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatDividerModule} from "@angular/material/divider";
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [
+    MatButtonModule, MatDividerModule, MatIconModule
+  ],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
