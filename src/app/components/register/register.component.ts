8d50f90b8cbc2f7f86b5e3e988c22b43b3e82502
Manon Locht <mlocht@outlook.com>
2024-01-16 19:30:16 +0100

    Add email, password, confirm password validation, match wireframe ui

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -5,12 +5,14 @@ import {MatInputModule} from '@angular/material/input';
 import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup, MinValidator} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import {FormValidator} from "./form-validator";
+import {MatCardModule} from "@angular/material/card";
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule,FormsModule, ReactiveFormsModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatCardModule],
 })
 export class RegisterComponent {
   hide = true;
@@ -19,16 +21,14 @@ export class RegisterComponent {
   password = new FormControl('', [Validators.required])
   passwordConfirm = new FormControl('', [Validators.required])
 
-
-  getErrorMessage() {
-    if (this.email.hasError('required') || this.displayName.hasError('required') || this.password.hasError('required') || this.passwordConfirm.hasError('required') )
-    {
-      return 'You must enter a value';
-    }
-
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
+  registerForm = new FormGroup({
+    email: this.email,
+    displayName: this.displayName,
+    password: this.password,
+    passwordConfirm: this.passwordConfirm
+  },
+    { validators: FormValidator.passwordsMatch}
+    )
 
 
   register() {
