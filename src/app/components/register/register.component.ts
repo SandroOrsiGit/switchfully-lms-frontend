672a141940449e6bc1038ca3d64321e9de2e90d1
Manon Locht <mlocht@outlook.com>
2024-01-16 17:31:46 +0100

    Add required field validation

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup} from '@angular/forms';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup, MinValidator} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 @Component({
@@ -16,38 +14,25 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class RegisterComponent {
   hide = true;
-  registerMessage = 'Registered Successfully';
   email = new FormControl('', [Validators.required, Validators.email]);
+  displayName = new FormControl('', [Validators.required])
+  password = new FormControl('', [Validators.required])
+  passwordConfirm = new FormControl('', [Validators.required])
+
 
   getErrorMessage() {
-    if (this.email.hasError('required')) {
+    if (this.email.hasError('required') || this.displayName.hasError('required') || this.password.hasError('required') || this.passwordConfirm.hasError('required') )
+    {
       return 'You must enter a value';
     }
+
 
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
-  registerForm = new FormGroup(
-    {
-      username: new FormControl(null, [Validators.required]),
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(6),
-      ]),
-      passwordConfirm: new FormControl(null, [Validators.required]),
-    },
-    
-  );
-
-  constructor(
-    private router: Router,
-   
-    private snackBar: MatSnackBar,
-   
-  ) {}
 
   register() {
-   
+
   }
-  
+
 }
