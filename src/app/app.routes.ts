c41a417b9fdf3495e5e5644985bf2b0dc78a41c9
Thomas Artois <thomas_artois99@hotmail.com>
2024-01-16 14:58:20 +0100

    Removed placeholder from app component html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {CreateClassgroupComponent} from "./pages/create-classgroup/create-classgroup.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'create-classgroup', component: CreateClassgroupComponent}
+];
