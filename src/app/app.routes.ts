3a80eaf20072ecb43451a1e4ea13dce9719efe63
Balder Bongaerts <bbongaerts@gmail.com>
2024-01-17 09:33:16 +0100

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {RegisterComponent} from "./components/register/register.component";
+import { LoginComponent } from './pages/login/login.component';
 
 
 export const routes: Routes = [
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent}
 ];
