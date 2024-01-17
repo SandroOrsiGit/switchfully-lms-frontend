4e2a8c97cd60634a3271631a222651d7511e3af7
Dylan Marvin <dylan.marvin@hotmail.be>
2024-01-17 10:42:02 +0100

    home page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import {RegisterComponent} from "./components/register/register.component";
+import { HomeComponent } from './pages/home/home/home.component';
 
 
 export const routes: Routes = [
-  {path: 'register', component: RegisterComponent}
+  {path: '', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'home', component: HomeComponent}
 ];
