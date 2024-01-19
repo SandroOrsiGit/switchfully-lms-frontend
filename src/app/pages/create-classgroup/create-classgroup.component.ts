ecd0c404c67195b1e22db68ae69da4107c0ef456
Karel Van Geerdeghom <karel.van.geerdeghom@gmail.com>
2024-01-19 16:23:46 +0100

    Add start- and enddate to create classgroup

--- src/app/pages/create-classgroup/create-classgroup.component.ts
+++ src/app/pages/create-classgroup/create-classgroup.component.ts
@@ -6,13 +6,16 @@ import {ButtonComponent} from "../../components/button/button.component";
 import {CommonModule} from "@angular/common";
 import {ClassgroupService} from "../../services/classgroup.service";
 import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
+import {MatCardModule} from "@angular/material/card";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MAT_DATE_LOCALE, MatNativeDateModule} from "@angular/material/core";
 
 @Component({
   selector: 'app-create-classgroup',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, ButtonComponent, ReactiveFormsModule, CommonModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ButtonComponent, ReactiveFormsModule, CommonModule, MatCardModule, MatDatepickerModule, MatNativeDateModule],
   templateUrl: './create-classgroup.component.html',
-  styleUrl: './create-classgroup.component.css'
+  styleUrl: './create-classgroup.component.css',
 })
 export class CreateClassgroupComponent implements OnInit{
   buttonName = '';
@@ -24,14 +27,16 @@ export class CreateClassgroupComponent implements OnInit{
   ngOnInit(): void {
     this.buttonName = 'Submit';
     this.createClassgroupForm = this.formBuilder.group( {
-      name: ['']
+      name: ['name'],
+      startDate: [''],
+      endDate: ['']
     });
   }
 
   onSubmit() {
     console.log("submit button clicked")
     let newClassgroup = this.createClassgroupForm.value;
-    // console.log("create onsubmit: ",newClassgroup);
+    console.log("create onsubmit: ",newClassgroup);
     this.classgroupService.createClassgroup(newClassgroup)
           .subscribe(response => console.log("created",response));
 
--- src/app/services/classgroup.service.ts
+++ src/app/services/classgroup.service.ts
@@ -8,7 +8,7 @@ import {Observable} from "rxjs";
   providedIn: 'root'
 })
 export class ClassgroupService {
-  private readonly url: string  = `${environment.backendUrl}/classgroup/create-classgroup`
+  private readonly url: string  = `${environment.backendUrl}/classgroup`
 
   private http: HttpClient = inject(HttpClient);
   constructor() { }
