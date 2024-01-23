3658d2410a4f178b386c42675e9c3f3ad5e847e8
Sandro Orsi <orsi.sandro.0@gmail.com>
2024-01-23 11:36:50 +0100

    push failing test to debug

--- src/app/pages/profile/profile.component.spec.ts
+++ src/app/pages/profile/profile.component.spec.ts
@@ -6,16 +6,20 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {Router} from '@angular/router';
 import {KeycloakService} from '../../services/keycloak.service';
 import {ChangeDetectionStrategy} from '@angular/core';
+import {UserService} from '../../services/user.service';
+import {User} from '../../models/User';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
   let keyCloakServiceMock: jasmine.SpyObj<KeycloakService>
   let routerMock: jasmine.SpyObj<Router>
+  let userServiceMock: jasmine.SpyObj<UserService>
 
   beforeEach(async () => {
-    keyCloakServiceMock = jasmine.createSpyObj('KeycloakService', ['isLoggedIn'])
-    routerMock = jasmine.createSpyObj('Router', ['navigate'])
+    keyCloakServiceMock = jasmine.createSpyObj('KeycloakService', ['isLoggedIn']);
+    routerMock = jasmine.createSpyObj('Router', ['navigate']);
+    userServiceMock = jasmine.createSpyObj('userService', ['getCurrentUser'])
     await TestBed.configureTestingModule({
       imports: [
         ProfileComponent,
@@ -24,10 +28,11 @@ describe('ProfileComponent', () => {
       ],
       providers: [
         {provide: KeycloakService, useValue: keyCloakServiceMock},
-        {provide: Router, useValue: routerMock}
+        {provide: Router, useValue: routerMock},
+        {provide: UserService, useValue: userServiceMock},
       ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
@@ -46,22 +51,30 @@ describe('ProfileComponent', () => {
     expect(routerMock.navigate).toHaveBeenCalledWith(['/login']);
   })
 
-  it('should initialize form controls with user data', fakeAsync(() => {
-    const userData = {
+  it('should initialize form controls with user data', () => {
+    keyCloakServiceMock.isLoggedIn.and.returnValue(true);
+    const userData: User = {
       id: 123,
       email: 'test@example.com',
       displayName: 'Test User',
       password: 'password123',
     };
+    userServiceMock.getCurrentUser.and.returnValue(userData)
 
-    component.user = userData;
+    component.ngOnInit();
     fixture.detectChanges();
-    tick();
+
+    // component.id.setValue(userData.id);
+    // component.email.setValue(userData.email);
+    // component.displayName.setValue(userData.displayName);
+    // component.password.setValue(userData.password);
+    // component.passwordConfirm.setValue(userData.password);
 
     expect(component.updateProfileForm.value.id).toEqual(userData.id);
     expect(component.email.value).toEqual(userData.email);
     expect(component.displayName.value).toEqual(userData.displayName);
     expect(component.password.value).toEqual(userData.password);
     expect(component.passwordConfirm.value).toEqual(userData.password);
-  }));
+    expect(userServiceMock.getCurrentUser).toHaveBeenCalled();
+  });
 });
