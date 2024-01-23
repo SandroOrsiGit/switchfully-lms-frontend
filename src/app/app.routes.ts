8c458ce29d45ee1f6b0a202b7139727193b6d89c
Balder Bongaerts <bbongaerts@gmail.com>
2024-01-23 10:36:27 +0100

    fix: refactor register page to pages dir

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import {RegisterComponent} from "./components/register/register.component";
+import {RegisterComponent} from "./pages/register/register.component";
 import { LoginComponent } from './pages/login/login.component';
 import {ProfileComponent} from './pages/profile/profile.component';
 
