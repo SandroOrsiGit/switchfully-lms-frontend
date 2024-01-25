ee2447bfe997a96b0b69b96826e121c4d2440faf
Wietse Pinoy <wietsepinoy@gmail.com>
2024-01-25 15:16:49 +0100

    guard url based on logged in student/coach

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,7 +2,7 @@ import { CanActivateFn } from '@angular/router';
 import {inject} from "@angular/core";
 import {UserService} from "../services/user.service";
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = () => {
   const userService  = inject(UserService);
   const currentUser = userService.getCurrentUser()
   return (!!currentUser);
--- src/app/guards/coach.guard.ts
+++ src/app/guards/coach.guard.ts
@@ -2,8 +2,7 @@ import { CanActivateFn } from '@angular/router';
 import {UserService} from "../services/user.service";
 import {inject} from "@angular/core";
 
-export const coachGuard: CanActivateFn = (
-  route, state) => {
+export const coachGuard: CanActivateFn = () => {
   const userService  = inject(UserService);
   const currentUser = userService.getCurrentUser()
   return (currentUser?.role == 'coach');
