9be34c67d7b339b04438f954e948d308a5027d9d
Karel Van Geerdeghom <karel.van.geerdeghom@gmail.com>
2024-01-26 15:04:22 +0100

    Add course overview

---
+++ src/app/pages/course-overview/course-overview.component.ts
@@ -0,0 +1,36 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {CourseDto} from "../../dtos/CourseDto";
+import {CourseService} from "../../services/course.service";
+import {MatTableModule} from "@angular/material/table";
+import {MatCardModule} from "@angular/material/card";
+
+@Component({
+  selector: 'app-course-overview',
+  standalone: true,
+  imports: [
+    MatTableModule,
+    MatCardModule
+  ],
+  templateUrl: './course-overview.component.html',
+  styleUrl: './course-overview.component.css'
+})
+export class CourseOverviewComponent implements OnInit {
+  private _courses: CourseDto[] = [];
+  private _courseService: CourseService = inject(CourseService);
+  displayedColumns: string[] = ['id', 'name'];
+
+  ngOnInit() {
+    this.getCourses();
+  }
+
+  private getCourses() {
+    this._courseService.getCourses().subscribe({
+      next: courses => this._courses = courses,
+      complete: () => console.log(this._courses)
+    });
+  }
+
+  get courses(): CourseDto[] {
+    return this._courses;
+  }
+}
