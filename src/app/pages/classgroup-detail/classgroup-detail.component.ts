9d8bd6a3ab8706fbb1b883419caf216de90942e7
Manon Locht <mlocht@outlook.com>
2024-02-01 15:57:34 +0100

    add filter (doesn't work)

--- src/app/pages/classgroup-detail/classgroup-detail.component.ts
+++ src/app/pages/classgroup-detail/classgroup-detail.component.ts
@@ -8,16 +8,25 @@ import {User} from "../../models/User";
 import {MatTableModule} from "@angular/material/table";
 import {MatCardModule} from "@angular/material/card";
 import {ButtonComponent} from "../../components/button/button.component";
+import {MatInputModule} from "@angular/material/input";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {StudentDto} from "../../dtos/StudentDto";
+import {map, Observable, startWith} from "rxjs";
 
 @Component({
   selector: 'app-classgroup-overview',
   standalone: true,
   imports: [CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterLink,
     MatTableModule,
     MatCardModule,
     ButtonComponent,
-    AsyncPipe
+    AsyncPipe,
+    MatInputModule,
+    MatAutocompleteModule
   ],
   templateUrl: './classgroup-detail.component.html',
   styleUrl: './classgroup-detail.component.css'
@@ -32,6 +41,12 @@ export class ClassgroupDetailComponent implements OnInit {
 
   private id: number = 0;
   private _classGroup: ClassGroup;
+  private _options: StudentDto[] = [];
+  myControl = new FormControl('');
+  btn_add_student: string = "Add To Classgroup";
+
+  filteredOptions: Observable<StudentDto[]>;
+
   public loggedInUser: User | undefined;
   btn_view_details: string = "View details";
 
@@ -46,6 +61,13 @@ export class ClassgroupDetailComponent implements OnInit {
       }
     })
 
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+
+    this.getStudents();
+
     this.loggedInUser = this.userService.getCurrentUser();
   }
 
@@ -53,8 +75,32 @@ export class ClassgroupDetailComponent implements OnInit {
     return this.loggedInUser?.role === 'coach';
   }
 
+  addStudent() {
+  }
 
+  private getStudents() {
+    this.userService.getAllStudents().subscribe(
+      {
+        next: studentDtoList => {
+          this._options = studentDtoList
+          console.log(this._options)
+        },
+        error: error => {
+          console.error(error)
+        }
+      }
+    )
+  }
+
+  private _filter(value: string): StudentDto[] {
+    const filterValue = value.toLowerCase();
+    return this._options.filter(option => option.displayName.toLowerCase().includes(filterValue));
+  }
   get classGroup(): ClassGroup {
     return this._classGroup;
+  }
+
+  get options(): StudentDto[] {
+    return this._options;
   }
 }
