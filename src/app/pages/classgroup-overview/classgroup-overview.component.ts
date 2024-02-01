1299f7b66c22ebfc42dee2b37a00048f9351e238
Thomas Artois <thomas_artois99@hotmail.com>
2024-02-01 11:48:38 +0100

    view classgroup is different for coach and student

--- src/app/pages/classgroup-overview/classgroup-overview.component.ts
+++ src/app/pages/classgroup-overview/classgroup-overview.component.ts
@@ -2,18 +2,27 @@ import {Component, OnInit, inject} from '@angular/core';
 import {ClassGroupService} from '../../services/class-group.service';
 import {ActivatedRoute, Params, RouterLink} from '@angular/router';
 import {ClassGroup} from '../../models/ClassGroup';
-import {CommonModule} from '@angular/common';
+import {AsyncPipe, CommonModule} from '@angular/common';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatDividerModule} from '@angular/material/divider';
 import {UserService} from "../../services/user.service";
 import {User} from "../../models/User";
 import {MatTableModule} from "@angular/material/table";
 import {MatCardModule} from "@angular/material/card";
+import {ButtonComponent} from "../../components/button/button.component";
 
 @Component({
   selector: 'app-classgroup-overview',
   standalone: true,
-  imports: [CommonModule, MatExpansionModule, MatDividerModule, RouterLink, MatTableModule, MatCardModule],
+  imports: [CommonModule,
+    MatExpansionModule,
+    MatDividerModule,
+    RouterLink,
+    MatTableModule,
+    MatCardModule,
+    ButtonComponent,
+    AsyncPipe
+  ],
   templateUrl: './classgroup-overview.component.html',
   styleUrl: './classgroup-overview.component.css'
 })
@@ -23,10 +32,15 @@ export class ClassgroupOverviewComponent implements OnInit {
   private activeRoute: ActivatedRoute = inject(ActivatedRoute);
   private userService: UserService = inject(UserService);
 
+  displayedCoachColumns: string[] = ['name', 'e-mail'];
+  displayedStudentForStudentColumns: string[] = ['name', 'e-mail'];
+  displayedStudentForCoachColumns: string[] = ['name', 'e-mail', 'view-details'];
+
   private id: number = 0;
-  public classGroup: ClassGroup;
+  private _classGroup: ClassGroup;
   public loggedInUser: User | undefined;
   panelOpenState = true;
+  btn_view_details: string = "View details";
 
   ngOnInit(): void {
     this.activeRoute.params.subscribe(
@@ -35,7 +49,7 @@ export class ClassgroupOverviewComponent implements OnInit {
       });
     this.classGroupService.getClassGroupByClassGroupId(this.id).pipe().subscribe({
       next: (classGroup) => {
-        this.classGroup = classGroup;
+        this._classGroup = classGroup;
       }
     })
 
@@ -44,5 +58,10 @@ export class ClassgroupOverviewComponent implements OnInit {
 
   isCoach(): boolean {
     return this.loggedInUser?.role === 'coach';
+  }
+
+
+  get classGroup(): ClassGroup {
+    return this._classGroup;
   }
 }
