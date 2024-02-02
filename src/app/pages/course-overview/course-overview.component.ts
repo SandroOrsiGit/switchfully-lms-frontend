cb627447258fc9a6020222337b1dbb61d283f873
Karel Van Geerdeghom <karel.van.geerdeghom@gmail.com>
2024-02-02 13:25:48 +0100

    Remove id columns from course and module overview

--- src/app/pages/course-overview/course-overview.component.ts
+++ src/app/pages/course-overview/course-overview.component.ts
@@ -25,7 +25,7 @@ export class CourseOverviewComponent implements OnInit {
   private _courses: CourseDto[] = [];
   private _courseService: CourseService = inject(CourseService);
   private _userService: UserService = inject(UserService);
-  displayedColumns: string[] = ['id', 'name', 'details', 'create_module'];
+  displayedColumns: string[] = ['name', 'details', 'create_module'];
   btn_create_course: string = "Create Course";
   btn_create_module: string = "Create Module";
   btn_edit_course: string = "Edit Course";
