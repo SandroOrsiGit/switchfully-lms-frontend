c3f124e04bdad4dd701c16307ae9cb0c0c11e041
Jussi Ccoyllo <jussi.ccoyllo@gmail.com>
2024-02-06 15:43:16 +0100

    add style navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,11 +6,12 @@ import {NgIf} from "@angular/common";
 import { KeycloakService } from '../../services/keycloak.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, RouterLink, NgIf, MatButtonModule, MatMenuModule],
+  imports: [MatToolbarModule, RouterLink, NgIf, MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
@@ -18,7 +19,7 @@ export class NavbarComponent {
     userService = inject(UserService);
     keycloackService = inject(KeycloakService);
   message() {
-    return 'Logged in as ' + this.userService.getCurrentUser()?.displayName;
+    return 'Welcome ' + this.userService.getCurrentUser()?.displayName;
   }
 
 }
