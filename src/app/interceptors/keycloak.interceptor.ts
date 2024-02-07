76eda87c1dbc37637ba991e50bb0fd4d4c811723
Sandro Orsi <orsi.sandro.0@gmail.com>
2024-02-07 13:48:06 +0100

    update intercepter url hardcoded

--- src/app/interceptors/keycloak.interceptor.ts
+++ src/app/interceptors/keycloak.interceptor.ts
@@ -1,16 +1,15 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-import { inject } from "@angular/core";
-import { KeycloakService } from '../services/keycloak.service';
-import { environment } from '../../environments/environment.dev';
+import {HttpInterceptorFn} from '@angular/common/http';
+import {inject} from '@angular/core';
+import {KeycloakService} from '../services/keycloak.service';
+import {environment} from '../../environments/environment.dev';
 
 export const keycloakInterceptor: HttpInterceptorFn = (req, next) => {
   const keycloakService = inject(KeycloakService);
 
   if (req.url.includes(`${environment.backendUrl}`)
     && keycloakService.getToken()
-    && req.url !== 'http://localhost:8080/user/register'
-    /* && 'https://keycloak.switchfully.com/realms/java-2023-10/protocol/openid-connect/token' */
-    ) {
+    && req.url !== `${environment.backendUrl}/user/register`
+  ) {
     req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${keycloakService.getToken()}`
