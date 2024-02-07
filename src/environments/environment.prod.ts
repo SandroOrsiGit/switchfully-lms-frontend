b98a7244f3778aae206e3fcfcd20f4021458b420
Sandro Orsi <orsi.sandro.0@gmail.com>
2024-02-07 14:37:24 +0100

    update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl: 'BACKEND_URL',
-  keycloakUrl: 'KEYCLOAK_URL',
+  backendUrl: process.env['NG_APP_BACKEND_URL'],
+  keycloakUrl: process.env['NG_APP_KEYCLOAK_URL'],
 };
